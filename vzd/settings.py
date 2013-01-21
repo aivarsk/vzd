8bcc44f8d21e771b4d7fea6ad3ff625a5a68e25f
Aivars Kalvans <aivars.kalvans@gmail.com>
2013-01-22 00:59:54 +0200

    scrapy template

---
+++ vzd/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for vzd project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'vzd'
+
+SPIDER_MODULES = ['vzd.spiders']
+NEWSPIDER_MODULE = 'vzd.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'vzd (+http://www.yourdomain.com)'
